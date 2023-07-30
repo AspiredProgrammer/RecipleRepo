a03ebc76b99e179e00a548548e25628e981d5a39
liploji <zeldrix.don@gmail.com>
2023-07-30 12:48:56 -0400

    Added search bar to search

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import SearchBar from "./SearchBar";
 
 const Search = () => {
   return (
     <div>
+      <SearchBar/>
       <h1>Welcome to the Search Page</h1>
       <p>This is the Search page of your application.</p>
     </div>
