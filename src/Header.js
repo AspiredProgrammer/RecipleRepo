b60e0e9b5c18d1197aa7143aa3422d02b9be5c47
liploji <58907680+liploji@users.noreply.github.com>
2023-07-30 03:16:54 +0000

    Created Header file and started on styling

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './styles.scss';
+
+function Header() {
+    return (
+        <div className="Header">
+            
+                Header
+            
+        </div>
+     );
+}
+
+
+export default Header;
